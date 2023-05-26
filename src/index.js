f19e8bddb83d7acba1d689225a70bafa0511b3a5
mudabir <96955114+Mudabir-NaQvi@users.noreply.github.com>
2023-05-26 12:56:58 +0500

    Updated code

--- src/index.js
+++ src/index.js
@@ -37,10 +37,6 @@ app.get("/", (req, res) => {
   res.redirect("/api/v1/");
 });
 
-// // homepage
-// app.get("/api/v1/", authenticate, async (req, res) => {
-//   res.send("Welcome to Homepage");
-// });
 
 // not found page
 app.get("/api/v1/*", (req, res) => {
